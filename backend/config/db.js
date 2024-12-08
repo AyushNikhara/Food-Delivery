790b0d96c734359ec82b8a5a1d4b6e8af7ecce02
Ayush Nikhara <ayushnikhara2002@gmail.com>
2024-12-08 19:30:59 +0530

    db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect(`mongodb+srv://ayushnikhara2002:Ayush2002@e-commerce.t7veb.mongodb.net/?retryWrites=true&w=majority&appName=e-commerce`).then(() => console.log("DB connected"));
-    //yaha pr `` iske andar apna monogodburl dal deS
+    await mongoose.connect(process.env.MONGODB_URL).then(() => console.log("DB connected"));
 }
